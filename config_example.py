581eea77addb10549001db685c5e24aa81fe2b72
William Di Luigi <williamdiluigi@gmail.com>
2020-03-22 21:26:21 +0100

    Remove unnecessary config fields
    Also remove unnecessary file

--- config_example.py
+++ config_example.py
@@ -1,11 +1,8 @@
 # rename this file to config.py and edit the required variable
 
 # APPLICATION_ROOT = "/teams/"
-DATABASE_URI = 'postgresql://user:password@localhost:5432/itacpc'
+DATABASE_URI = "postgresql://user:password@localhost:5432/itacpc"
 SECRET_KEY = b"very secret key"  # os.urandom(16)
 
 # mail settings
-SMTP_SERVER = "smtp.sendgrid.net"
-SMTP_PORT = 465
-SMTP_USER = "apikey"
-SMTP_PASSWORD = ""
+SENDGRID_API_KEY = "here sendgrid api key"
