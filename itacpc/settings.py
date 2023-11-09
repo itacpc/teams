49cb8b2ff65221a47d678992fd4bbe09234b4464
William Di Luigi <williamdiluigi@gmail.com>
2023-11-09 23:05:13 +0100

    Prod DB settings

--- itacpc/settings.py
+++ itacpc/settings.py
@@ -93,11 +93,21 @@ WSGI_APPLICATION = "itacpc.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'dev': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'production': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    },
 }
+
+DATABASES['default'] = DATABASES['dev' if DEBUG else 'production']
 
 
 # Custom user
