66ca08c025190e5179872ab5b5928f859e3edb63
William Di Luigi <williamdiluigi@gmail.com>
2024-10-09 00:59:49 +0200

    Delete old version of the app

--- old_app/config/config.py
+++
@@ -1,8 +0,0 @@
-# rename this file to config.py and edit the required variable
-
-# APPLICATION_ROOT = "/teams/"
-DATABASE_URI = "postgresql://user:password@localhost:5432/itacpc"
-SECRET_KEY = b"very secret key"  # os.urandom(16)
-
-# mail settings
-SENDGRID_API_KEY = "here sendgrid api key"
